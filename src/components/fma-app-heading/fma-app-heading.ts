ecef9eb826c30b6dfb513469f5471f3e924ae123
HQ\swanepoelh <that.other.guy.i.know@gmail.com>
2018-08-21 01:10:21 +0200

    Collapsible app heading

--- src/components/fma-app-heading/fma-app-heading.ts
+++ src/components/fma-app-heading/fma-app-heading.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ScrollEvent } from 'ionic-angular';
+import { Observable } from 'rxjs';
 
 /**
  * Generated class for the FmaAppHeadingComponent component.
@@ -10,8 +12,30 @@ import { Component } from '@angular/core';
   selector: 'fma-app-heading',
   templateUrl: 'fma-app-heading.html'
 })
-export class FmaAppHeadingComponent {
+export class FmaAppHeadingComponent  {
+  // @Input('onScroll') onScroll: Observable<ScrollEvent>;
+  @Input('onScroll') set onScroll(event: Observable<ScrollEvent>) {
+    event.subscribe((scrollData: ScrollEvent) => {
+      if (this.appHeader) {
+        if (scrollData.scrollTop > 5)
+          this.appHeader.nativeElement.style.paddingTop = "0px";
 
-  constructor() { }
+        else
+        this.appHeader.nativeElement.style.paddingTop = "175px";
+      }
+    });
+  };
+  @Output('doSearch') doSearch: EventEmitter<any>;
+  @ViewChild('appHeader') appHeader: ElementRef;
+
+  searchText: string = '';
+
+  constructor() {
+    this.doSearch = new EventEmitter<any>();
+  }
+
+  onIonInput(event: any): void {
+    this.doSearch.emit(this.searchText);
+  }
 
 }
