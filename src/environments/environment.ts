03b0a7f68a63cf6b61e90ab0002421bbe190a12c
HQ\swanepoelh <that.other.guy.i.know@gmail.com>
2019-01-29 20:58:49 +0200

    Unamplify subscriber & services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,9 @@ export const ENV = {
     serviceIcons: 'https://d1x0w1iu1zaah0.cloudfront.net',
     avatars: 'https://d2awch9bb4c24l.cloudfront.net',
     covers: 'https://d1iugcxvqzee9a.cloudfront.net'
+  },
+  dataUrls: {
+    subscribers: 'https://dbep3uy1fc.execute-api.us-east-2.amazonaws.com/dev',
+    services: 'https://brblt7abh0.execute-api.us-east-2.amazonaws.com/dev'
   }
 }
